cd720295ba012b7136d144caffeb8da8d19c07ac
Uncleifechukwu <ifennaonukwuli@gmail.com>
2023-02-17 16:23:03 -0800

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+
+/**
+ * main - Print the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar("\n");
+	return (0);
+}
