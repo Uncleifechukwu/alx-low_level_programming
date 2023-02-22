911f2867ade5b218f3997d9cd33b4126a30cec9f
Uncleifechukwu <ifennaonukwuli@gmail.com>
2023-02-21 20:33:25 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,7 +4,7 @@
  * @n: the int to extract the last digit from
  * Return: value of the last digit
  */
-int print_last_digit(int n)
+	int print_last_digit(int n)
 {
 	int a;	
 
@@ -19,5 +19,4 @@ int print_last_digit(int n)
 	_putchar(a + '0');
 	
 	return (a);
-		
 }
